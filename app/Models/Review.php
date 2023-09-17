df41696b31b505fa4c50f0d09458801503049dab
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-17 12:12:56 +0200

    added fillable to reviews model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'webflow_user_id',
+        'product_id',
+        'string',
+    ];
 }
