f318694daa495b9e3b171fd87f10a89ac5342f11
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-18 23:33:53 +0200

    improved sign up form

--- public/user-signup.js
+++ public/user-signup.js
@@ -1,9 +1,9 @@
 form = document.querySelector('[data-wf-user-form-type="signup"]');
 
 form.addEventListener('submit', function () {
-    localStorage.setItem("email", email.value);
+    localStorage.setItem("email", document.querySelector('#wf-sign-up-email').value);
     localStorage.setItem("valid", true);
-    localStorage.setItem("name", name.value)
+    localStorage.setItem("name", document.querySelector('#wf-sign-up-name').value)
 
     fetch("https://app.unmxdern.com/api/new-user", {
         method: "POST",
