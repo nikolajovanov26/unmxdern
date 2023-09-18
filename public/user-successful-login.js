5317f315f406b1fbd34aed3829fd4c407f827da1
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-18 23:40:40 +0200

    added functions for successfully log in

--- public/user-successful-login.js
+++ public/user-successful-login.js
@@ -1,6 +1,6 @@
 localStorage.setItem("valid", true);
 
-fetch("https://app.unmxdern.com/api/user-data")
+fetch("https://app.unmxdern.com/api/user-data?email=" . localStorage.getItem("email"))
     .then((response) => response.json())
     .then((json) => console.log(json));
 
