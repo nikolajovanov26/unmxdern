ba8f353068574066a8645eea07343ac6b009d1c9
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-19 22:49:57 +0200

    removed migration for product slug

--- database/migrations/2023_09_13_195411_create_favorite_product_table.php
+++ database/migrations/2023_09_13_195411_create_favorite_product_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('slug')->after('name');
+        Schema::create('favorite_product', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('slug');
-        });
+        Schema::dropIfExists('favorite_product');
     }
 };
