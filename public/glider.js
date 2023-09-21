c5b06d024de2f54cc8a84462c40445dacf573f23
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-21 20:44:02 +0200

    if a product is not found try to sync from webflow

--- public/glider.js
+++ public/glider.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let glide = document.querySelector('.glide');
-    let removedScripts = [];
+    createGlide()
+
+    document.querySelector('#option-set-6880adb96d8fc51c4b67e9e4ce4bce7e').addEventListener('change', function () {
+        createGlide()
+    });
+});
+
+function createGlide()
+{
+    glide = document.querySelector('.glide');
+    removedScripts = [];
 
     if (glide) {
-        let slidesDiv = document.querySelector('.glide__slides');
+        slidesDiv = document.querySelector('.glide__slides');
 
-        // Remove the script tags and store them in the removedScripts array
         Array.from(slidesDiv.parentElement.childNodes).forEach(node => {
             if (node.tagName === 'SCRIPT') {
                 removedScripts.push(node);
@@ -13,11 +21,16 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
 
-        var navigation = '<div class="glide__bullets" data-glide-el="controls[nav]">';
-        var slideNum = 0;
+        bullets = document.querySelector('.glide__bullets');
+        if (bullets !== null) {
+            bullets.innerHTML = '';
+        }
+
+        navigation = '<div class="glide__bullets" data-glide-el="controls[nav]">';
+        slideNum = 0;
 
         document.querySelectorAll('.glide__slide').forEach(slide => {
-            var imgSrc = slide.querySelector('img').src;
+            imgSrc = slide.querySelector('img').src;
             navigation += '<button class="glide__bullet" data-glide-dir="=' + slideNum + '" style="background-image:url(' + imgSrc + ')"></button>';
             slideNum++;
         });
@@ -36,16 +49,12 @@ document.addEventListener("DOMContentLoaded", () => {
             rewindDuration: 1600,
         };
 
-        const products = new Glide(".glide", settings);
+        var products = new Glide(".glide", settings);
 
         products.mount();
 
-        // Listen for the 'run.end' event, which is triggered when the Glide component has finished mounting
-        products.on('run.end', () => {
-            // Add the removed script tags back to their original place
-            removedScripts.forEach(script => {
-                slidesDiv.parentElement.appendChild(script);
-            });
+        removedScripts.forEach(script => {
+            slidesDiv.parentElement.appendChild(script);
         });
     }
-});
+}
