42e5094c9f21831ac7ba473c1c7bd6544351177b
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-09-22 21:44:33 +0200

    added adjustments on the glider js

--- public/glider.js
+++ public/glider.js
@@ -10,6 +10,8 @@ document.querySelector('#option-set-6880adb96d8fc51c4b67e9e4ce4bce7e').addEventL
     addZoomInEffect()
 });
 
+document.querySelector('#option-set-6880adb96d8fc51c4b67e9e4ce4bce7e').click()
+
 function createGlide() {
     glide = document.querySelector('.glide');
     removedScripts = [];
